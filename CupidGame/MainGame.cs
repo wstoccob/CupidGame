42019b772106e734a65b3248261ce4fb4671eb0b
wstoccob <alinur.seysekov@gmail.com>
2024-02-12 21:10:56 +0600

    Add Rendering logic

--- CupidGame/MainGame.cs
+++ CupidGame/MainGame.cs
@@ -23,8 +23,13 @@ public class MainGame : Game
 
     protected override void Initialize()
     {
-        _canvas = new Canvas(_graphics.GraphicsDevice, 300, 200);
+        _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+        _graphics.ApplyChanges();
+        
+        _canvas = new Canvas(_graphics.GraphicsDevice, 960, 540);
         _sprite = new Sprite(Content.Load<Texture2D>("mainMenu"), new Vector2(0, 0), 0);
+        _canvas.SetDestinationRectangle();
         base.Initialize();
     }
 
@@ -45,10 +50,11 @@ public class MainGame : Game
     protected override void Draw(GameTime gameTime)
     {
         _canvas.Activate();
-        _spriteBatch.Begin();
+        _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
         // TODO: add your drawing logic to the target
         _sprite.Render(_spriteBatch);
         _spriteBatch.End();
+        
         _canvas.Draw(_spriteBatch);
         
         base.Draw(gameTime);
