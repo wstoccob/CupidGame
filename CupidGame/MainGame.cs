5702aae771c5c9751582f3ec53eede48212eb6dc
wstoccob <alinur.seysekov@gmail.com>
2024-02-13 20:56:28 +0600

    Add mainMenu and hearts

--- CupidGame/MainGame.cs
+++ CupidGame/MainGame.cs
@@ -1,4 +1,6 @@
-﻿using CupidGame.Objects;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CupidGame.Objects;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -11,7 +13,12 @@ public class MainGame : Game
     private SpriteBatch _spriteBatch;
 
     private Canvas _canvas;
-    private Sprite _sprite;
+    private List<Sprite> _spriteList;
+
+    private Sprite _mainMenu;
+    private Sprite _heartOne;
+    private Sprite _heartTwo;
+    private Sprite _menuText;
 
     public MainGame()
     {
@@ -27,8 +34,13 @@ public class MainGame : Game
         _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
         _graphics.ApplyChanges();
         
-        _canvas = new Canvas(_graphics.GraphicsDevice, 960, 540);
-        _sprite = new Sprite(Content.Load<Texture2D>("mainMenu"), new Vector2(0, 0), 0);
+        _canvas = new Canvas(_graphics.GraphicsDevice, 1920, 1080);
+        _spriteList = new List<Sprite>();
+        _mainMenu = new Sprite(LoadTexture("mainMenu"), new Vector2(0, 0), 0);
+        _heartOne = new Sprite(LoadTexture("heart"), new Vector2(10, 10), 1);
+        _heartTwo = new Sprite(LoadTexture("heart"), new Vector2(500, 500), 1);
+        _mainText = 
+        
         _canvas.SetDestinationRectangle();
         base.Initialize();
     }
@@ -36,6 +48,9 @@ public class MainGame : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _spriteList.Add(_mainMenu);
+        _spriteList.Add(_heartOne);
+        _spriteList.Add(_heartTwo);
     }
 
     protected override void Update(GameTime gameTime)
@@ -51,12 +66,21 @@ public class MainGame : Game
     {
         _canvas.Activate();
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
-        // TODO: add your drawing logic to the target
-        _sprite.Render(_spriteBatch);
+
+        foreach (var sprite in _spriteList.OrderBy(a => a._zIndex))
+        {
+            sprite.Render(_spriteBatch);
+        }
+        
         _spriteBatch.End();
         
         _canvas.Draw(_spriteBatch);
         
         base.Draw(gameTime);
+    }
+
+    private Texture2D LoadTexture(string assetName)
+    {
+        return Content.Load<Texture2D>(assetName);
     }
 }
