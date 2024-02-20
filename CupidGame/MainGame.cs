3dc005240b0452560c350ab05ce21109bb7ae082
wstoccob <alinur.seysekov@gmail.com>
2024-02-20 11:01:02 +0600

    Last commit

--- CupidGame/MainGame.cs
+++ CupidGame/MainGame.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Net.Sockets;
 using CupidGame.Objects;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
